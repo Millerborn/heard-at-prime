171650bce503b0fd9f641220f9ac43ec4fe512ac
Cooper Miller <coopermllr@gmail.com>
2018-10-25 22:21:01 -0500

    Database file

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,5 +3,25 @@ console.log('JS');
 $(document).ready(onReady);
 
 function onReady(){
-    console.log('JQ');  
+    console.log('JQ'); 
+    getQuoteData();
+}
+
+// get artist data from the server
+function getQuoteData() {
+    $.ajax({
+        type: 'GET',
+        url: '/quotes'
+    }).then(function (response) {
+        // append data to the DOM
+        for (let i = 0; i < response.length; i++) {
+            let quote = response[i];
+            $('#front').append(`
+                    <p>${quote.name}</p>
+            `);
+            $('#back').append(`
+            <p>${quote.name}</p>
+    `);
+        }
+    });
 }
