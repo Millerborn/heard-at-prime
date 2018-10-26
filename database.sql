88f3ca827cc97dcaa279429889c40014e7b2d26f
Cooper Miller <coopermllr@gmail.com>
2018-10-25 22:25:24 -0500

    Database file

---
+++ database.sql
@@ -0,0 +1,40 @@
+CREATE TABLE quotes (
+id serial primary key,
+quote varchar(160)
+);
+
+-- run the following inserts to populate your current inventory
+
+INSERT INTO quotes (quote) 
+VALUES ('You sound like a Front End Developer.');
+INSERT INTO quotes (quote) 
+VALUES ('You control the server, you don’t control the clients.');
+INSERT INTO quotes (quote) 
+VALUES ('Curly brackets plus the backticks makes this not jQuery, it’s just Javascript.');
+INSERT INTO quotes (quote) 
+VALUES ('You’re not a real web server.');
+INSERT INTO quotes (quote) 
+VALUES ('s is a very important character.');
+INSERT INTO quotes (quote) 
+VALUES ('That’s my big motto: Let’s get some shit done.');
+INSERT INTO quotes (quote) 
+VALUES ('Bootstrap, it’s a thing - Dev.');
+INSERT INTO quotes (quote) 
+VALUES ('It sounded like a garbage version of Operation for a second.');
+INSERT INTO quotes (quote) 
+VALUES ('And that’s a single quotes maneuver there?.');
+INSERT INTO quotes (quote) 
+VALUES ('I don’t care if my love is capitalized or not.');
+INSERT INTO quotes (quote) 
+VALUES ('You can think of how you interact with things and use them in many different contexts outside of code.');
+INSERT INTO quotes (quote) 
+VALUES ('Do you have experience ruining peoples lives? Because you seem pretty good at it.');
+INSERT INTO quotes (quote) 
+VALUES ('The stuff in the GUI, it changes.');
+INSERT INTO quotes (quote) 
+VALUES ('Once you know how it works, sure make your life easier, but for now… struggle through!');
+INSERT INTO quotes (quote) 
+VALUES ('I’ve always waited for the day my classroom would become an illegal gambling den.');
+INSERT INTO quotes (quote) 
+VALUES ('I’m already googling it in my mind.');
+
